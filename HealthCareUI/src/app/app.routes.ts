6d008c2993b49ab4d59805e5f423e9621ae6de06
HarryNg59 <huyheo05092004@gmail.com>
2025-06-05 01:04:17 +0700

    update loginUI

--- HealthCareUI/src/app/app.routes.ts
+++ HealthCareUI/src/app/app.routes.ts
@@ -3,12 +3,19 @@ import { HomeComponent } from './feature/home/home.component';
 import { ConfirmEmailComponent, ForgotPasswordComponent, LoginComponent, RegistrationComponent, ResetPasswordComponent } from './feature/Auth';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  //cái này để trên cùng
   { path: '', redirectTo: 'home', pathMatch: 'full' },//mới khởi tạo đẩy về home
-  { path: '**', redirectTo: 'home' },//invalid đẩy về home
+  //pathMatch là khi đường dẫn URL là hoàn toàn rỗng (''), thì mới chuyển hướng sang /home.
+
+
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent},
   { path: 'registration', component: RegistrationComponent },
   { path: 'confirm-email', component: ConfirmEmailComponent },
-  { path: 'login', component: LoginComponent},
   { path: 'reset-password', component: ResetPasswordComponent},
-  { path: 'forgot-password', component:ForgotPasswordComponent}
-];
+  { path: 'forgot-password', component: ForgotPasswordComponent},
+
+
+  //cái này phải để cuối cùng
+  { path: '**', redirectTo: 'home' },//invalid đẩy về home
+];//ko chơi thay đổi thứ tự route nha mấy ní
