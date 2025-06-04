82c0fd47276e906887f856b7272c41f612d716ad
HarryNg59 <huyheo05092004@gmail.com>
2025-06-05 00:21:48 +0700

    update ui

--- HealthCareUI/src/app/app.routes.ts
+++ HealthCareUI/src/app/app.routes.ts
@@ -3,7 +3,9 @@ import { HomeComponent } from './feature/home/home.component';
 import { ConfirmEmailComponent, LoginComponent, RegistrationComponent, ResetPasswordComponent } from './feature/Auth';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },//mới khởi tạo đẩy về home
+  { path: '**', redirectTo: 'home' },//invalid đẩy về home
   { path: 'registration', component: RegistrationComponent },
   { path: 'confirm-email', component: ConfirmEmailComponent },
   { path: 'login', component: LoginComponent},
