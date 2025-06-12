b3307e7e53bba3aad63d2ab1baead68c0d9bcb09
HarryNg59 <huyheo05092004@gmail.com>
2025-06-12 16:28:25 +0700

    update environment onstant

--- HealthCareUI/src/app/app.config.ts
+++ HealthCareUI/src/app/app.config.ts
@@ -22,5 +22,5 @@ export const appConfig: ApplicationConfig = {
 };
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:5000'
+  apiUrl: 'http://localhost:5000'
 };
--- HealthCareUI/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-import { environment } from "../app/app.config";
-
-export const environments = {
-  production: false,
-  apiAuthUrl: environment + '/api/auth',
-  apiAccountsUrl: environment + '/api/accounts'
-};
