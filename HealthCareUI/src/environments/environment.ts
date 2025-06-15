431f9fe02cad625448f36b07ce437767f62bad13
HarryNg59 <huyheo05092004@gmail.com>
2025-06-15 14:39:40 +0700

    delete folder environment

--- HealthCareUI/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-import { environment } from '../app/app.config';
-
-export const environments = {
-  production: false,
-  apiAuthUrl: 'http://localhost:5000/api/Auth',
-  apiAccountsUrl: 'http://localhost:5000/api/accounts',
-};
