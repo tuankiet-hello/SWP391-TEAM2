f162ed060faff7e646d1e015740560d160814312
tuankiet-hello <tuankiet.08102004@gmail.com>
2025-07-10 13:35:31 +0700

    Create&Edit MenstrualCycle

--- HealthCareUI/src/services/menstrual-cycle.service.ts
+++ HealthCareUI/src/services/menstrual-cycle.service.ts
@@ -1,30 +1,41 @@
 import { MenstrualCycle } from './../app/feature/client/menstrual-cycle/created-menstrual-cycle/created-menstrual-cycle.component';
-import { Injectable } from "@angular/core";
-import { environment } from "../app/app.config";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { environment } from '../app/app.config';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 
 export interface MenstrualCycleDTO {
-    accountID: string;
-    start_date: string;
-    end_date: string;
-    reminder_enabled: number;
-    note: string;
+  cycleID: number;
+  accountID: string;
+  start_date: string;
+  end_date: string;
+  reminder_enabled: boolean;
+  note?: string;
 }
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class MenstrualService {
-    private apiUrl = environment.apiUrl + '/api/MenstrualCycle'; // Đổi thành API thực tế của cậu
+  private apiUrl = environment.apiUrl + '/api/MenstrualCycle'; // Đổi thành API thực tế của cậu
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    getCycleByAccId(id: string): Observable<MenstrualCycleDTO[]> {
-        return this.http.get<MenstrualCycleDTO[]>(`${this.apiUrl}/menstrual-cycle-by-account-id/${id}`);
-    }
-    createCycle(cycle: MenstrualCycleDTO): Observable<any> {
-        return this.http.post<any>(`${this.apiUrl}/add-menstrual-cycle`, cycle);
-    }
+  getCycleByAccId(id: string): Observable<MenstrualCycleDTO[]> {
+    return this.http.get<MenstrualCycleDTO[]>(
+      `${this.apiUrl}/menstrual-cycle-by-account-id/${id}`
+    );
+  }
+  createCycle(cycle: MenstrualCycleDTO): Observable<MenstrualCycleDTO> {
+    return this.http.post<MenstrualCycleDTO>(
+      `${this.apiUrl}/add-menstrual-cycle`,
+      cycle
+    );
+  }
 
-
+  editCycle(cycleID: number, cycle: MenstrualCycleDTO): Observable<any> {
+    return this.http.put(
+      `${this.apiUrl}/update-menstrual-cycle/${cycleID}`,
+      cycle
+    );
+  }
 }
